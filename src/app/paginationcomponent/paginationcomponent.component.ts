b61662a764471cb7a8426cb1c8adc0112b646848
Alena Fatianova <ravemybody@gmail.com>
2021-04-03 01:45:39 +0500

    create new paginator component

---
+++ src/app/paginationcomponent/paginationcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paginationcomponent',
+  templateUrl: './paginationcomponent.component.html',
+  styleUrls: ['./paginationcomponent.component.sass']
+})
+export class PaginationcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
